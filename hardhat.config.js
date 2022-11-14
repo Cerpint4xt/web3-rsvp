b3d0ad5cccea5f57b9ffc493d75c85a41f6ea87c
Cerpint4xt <rcatagrundy@gmail.com>
2022-11-14 18:00:36 +0000

    hardhatconfig update

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const STAGING_ALCHEMY_URL = process.env.STAGING_INFURA_URL;
 const STAGING_PRIVATE_KEY = process.env.STAGING_PRIVATE_KEY;
-
+/*
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,11 +16,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
+*/
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  solidity: "0.8.4",
   network:{
     hardhat:{
       chainId: 1337,
@@ -32,14 +33,4 @@ module.exports = {
       gasPrice: 8000000000,
     },
   },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  
 };
